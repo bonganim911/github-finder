13e4d7765b80b654edc0f54f809bbe5ab1db3771
bonganim911 <bonganim911@gmail.com>
2019-07-13 16:30:15 +0400

    Added init.

--- src/App.js
+++ src/App.js
@@ -1,26 +1,51 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import Navbar from './component/layout/Navbar'
+import Users from './component/users/User'
+import Search from './component/Search'
+import axios from 'axios';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+
+class App extends React.Component {
+
+    state = {
+        users: [],
+        loading: false
+    };
+
+    async componentDidMount() {
+        this.setState({loading: true});
+
+        const response = await axios.get(`https://api.github.com/users?client_id=${process.env.REACT_APP_GITHUB_CLIENT_ID}&client_secret=${process.env.REACT_APP__GITHUB_CLIENT_SECRET}`);
+        this.setState({users: response.data, loading: false});
+    }
+
+    clearState = () => {
+        this.setState({users: [], loading: false});
+    };
+
+    searchUser = async text => {
+        this.setState({loading: true});
+        const response = await axios.get(`https://api.github.com/search/users?q=${text}
+                                &client_id=${process.env.REACT_APP_GITHUB_CLIENT_ID}
+                                &client_secret=${process.env.REACT_APP__GITHUB_CLIENT_SECRET}`);
+        this.setState({users: response.data.items, loading: false});
+    };
+
+    render() {
+        const {users, loading} = this.state;
+        return (
+            <div className="App">
+                <Navbar/>
+                <Search  searchUsers={this.searchUser}
+                         clearUsers={this.clearState}
+                         showClear={users.length > 0}
+
+                />
+                <Users users={users} loading={loading}/>
+            </div>
+        );
+    };
+};
 
 export default App;
